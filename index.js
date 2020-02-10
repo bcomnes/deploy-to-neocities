7e95fe7e6884c2e824b71b2aeaf3fbebcb25f73f
Bret Comnes <bcomnes@gmail.com>
2020-02-10 12:35:36 -0700

    checking in

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
+const core = require('@actions/core')
+// const github = require('@actions/github')
+// const Neocities = require('async-neocities')
+const path = require('path')
+const exec = require('child_process').exec
 
-const { NeocitiesAPIClient } = require('./lib/client.js')
+async function doDeploy () {
+  // `who-to-greet` input defined in action metadata file
+  const token = core.getInput('api-token')
+  const distDir = core.getInput('dist-dir')
+  const cleanup = core.getInput('cleanup')
 
-module.exports = { NeocitiesAPIClient }
+  const time = (new Date()).toTimeString()
+  core.setOutput('time', time)
+
+  const client = new Neocities(token)
+
+  console.log(process.cwd())
+  console.log(path.join(process.cwd(), distDir))
+
+  return new Promise((resolve, reject) => {
+    exec('ls -la', (error, stdout, stderr) => {
+      console.log(stdout)
+      console.log(stderr)
+      if (error !== null) {
+        console.log(`exec error: ${error}`)
+      }
+      resolve()
+    })
+  })
+
+  // return client.deploy()
+}
+
+doDeploy.then(() => {}).catch(err => {
+  core.setFailed(err.message)
+})
