e49d0b8d3a015d0899690d04395c86ecbb1121dc
Bret Comnes <bcomnes@gmail.com>
2020-02-10 17:21:26 -0700

    Update deps

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core')
 // const github = require('@actions/github')
-const Neocities = require('async-neocities-tmp')
+const Neocities = require('async-neocities')
 const path = require('path')
 const exec = require('child_process').exec
 
